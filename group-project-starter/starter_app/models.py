19864a82ba9bf674b330e5aeeab05e77f0af6742
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-08-29 21:24:40 -0400

    some db work

--- group-project-starter/starter_app/models.py
+++ group-project-starter/starter_app/models.py
@@ -2,16 +2,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashedPassword = db.Column(db.Varbinary, nullable=False)
+    firstName = db.Column(db.String(40), nullable=False)
+    lastName = db.Column(db.String(40), nullable=False)
+    pinnedTweet = db.Column(db.Integer)
+    about = db.Column(db.Text(255))
+    profilePicture = db.Column(db.String(255))
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
+
+
+class Tweet(db.Model):
+    __tablename__ = 'tweets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String(140), nullable=False)
+    retweetedTweetId = db.Column(db.Integer)
