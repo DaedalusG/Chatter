7aa1f1ee817a541e1266ca8ebdae3f01fa022051
warrgiff <warrenbruceg@gmail.com>
2020-08-31 14:45:37 -0700

    fixed a thing in config

--- flask/config.py
+++ flask/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://twitter_user:password@http://18.217.151.140:80/twitter_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://twitter_user:password@18.217.151.140:80/twitter_db"
     # os.environ.get('FLASK_ENV')=="DEVELOPMENT"
     # if "postgresql://twitter_user:password@localhost:9876/twitter_db"
     # else os.environ.get('DATABASE_URL') )
