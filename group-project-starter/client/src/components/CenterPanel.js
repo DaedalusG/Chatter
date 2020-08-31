21c6eaf2b8af8ba5a604a6b1e712e60391cbd8fc
Casey <innerforest7@gmail.com>
2020-08-31 15:33:56 -0700

    add right panel content to mainPage

--- group-project-starter/client/src/components/CenterPanel.js
+++ group-project-starter/client/src/components/CenterPanel.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Sparkles from '../images/Sparkles';
+import Landcape from '../images/Landscape';
+import GifBox from '../images/GifBox';
+import PollBox from '../images/PollBox';
+import SmileyFace from '../images/SileyFace';
+import Calendar from '../images/Calendar'
+
 
 const CenterPanel = ()=>{
   return(
@@ -10,17 +16,26 @@ const CenterPanel = ()=>{
         <Sparkles></Sparkles>
       </div>
       <div id={"center-panel__below-nav"} >
-        <div id={"center-panel__below-nav__profile-bublle-c"}>
-          <div className={"profile-bubble-2"} ></div>
-        </div>
-          <div id={"center-panel__below-nav__content-c"}>
-            <div className={"below-nav-section"} >
-              {/* <textarea id={"tweet-textarea"} name={"tweet-textarea"} rows={1} cols={33} wrap={"soft"} resize={"none"} placeholder={"What's happening?"} ></textarea> */}
-              <span class="textarea" role="textbox" contenteditable=""></span>
-              
+      
+        {/* <div id={"center-panel__below-nav__content-c"}> */}
+          <div className={"below-nav-section"} >
+            {/* <textarea id={"tweet-textarea"} name={"tweet-textarea"} rows={1} cols={33} wrap={"soft"} resize={"none"} placeholder={"What's happening?"} ></textarea> */}
+            <div id={"center-panel__below-nav__profile-bublle-c"}>
+              <div className={"profile-bubble-2"} ></div>
             </div>
-            <div className={"below-nav-section"} ></div>
+            <span name="tweet-textarea" class="textarea" role="textbox" resize="none"  contenteditable=""></span>
           </div>
+          <div className={"below-nav-section-2"} >
+            <Landcape></Landcape>
+            <GifBox></GifBox>
+            <PollBox></PollBox>
+            <SmileyFace></SmileyFace>
+            <Calendar></Calendar>
+            <div id={"tweet-button-2"}>
+              <span>Tweet</span>
+            </div>
+          </div>
+        {/* </div> */}
       </div>
     </div>
     
--- group-project-starter/client/src/components/RightPanel.js
+++ group-project-starter/client/src/components/RightPanel.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import MagnifyingGlass from '../images/MagnifyingGlass';
+
 
 const RightPanel = ()=>{
   return(
-    <div id={"main-c__right"} ></div>
+    <div id={"main-c__right"} >
+      <div id={"main-c__right__search-bar"} >
+        <MagnifyingGlass></MagnifyingGlass>
+        <input id={"search-input"} name={"search-input"} placeholder="Search Chatter" ></input>
+      </div>
+      <div id={"main-c__right__event-c"} >
+        <div id={"main-c__right__event-c__top"} ><span>What's happening</span></div>
+        <div className={"main-c__right__event-c__news"}>
+          <div className={"news-feed"}>
+            <p>Heman Again</p>
+            <p>And battle cat!</p>
+          </div>
+          <div className={"news-pic"} ></div>
+        </div>
+        <div className={"main-c__right__event-c__news"}>
+          <div className={"news-feed"}>
+            <p>Heman Again</p>
+            <p>And battle cat!</p>
+          </div>
+          <div className={"news-pic"} ></div>
+        </div>
+        <div className={"main-c__right__event-c__news"}>
+          <div className={"news-feed"}>
+            <p>Heman Again</p>
+            <p>And battle cat!</p>
+          </div>
+          <div className={"news-pic"} ></div>
+        </div>
+        <div className={"main-c__right__event-c__news"}>
+          <div className={"news-feed"}>
+            <p>Heman Again</p>
+            <p>And battle cat!</p>
+          </div>
+          <div className={"news-pic"} ></div>
+        </div>
+        <div className={"main-c__right__event-c__news"}>
+          <div className={"news-feed"}>
+            <p>Heman Again</p>
+            <p>And battle cat!</p>
+          </div>
+          <div className={"news-pic"} ></div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default RightPanel;
