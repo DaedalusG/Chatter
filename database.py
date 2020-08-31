be128321566c9ef9678ec5e8464c5bc32f7ffb29
warrgiff <warrenbruceg@gmail.com>
2020-08-31 00:31:44 -0700

    working models for database, needs to be tested with seeding, may need to look at one to one relation for pinned tweet

--- database.py
+++ database.py
@@ -8,12 +8,18 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-    ian = User(username='Ian', email='ian@aa.io')
-    javier = User(username='Javier', email='javier@aa.io')
-    dean = User(username='Dean', email='dean@aa.io')
-    angela = User(username='Angela', email='angela@aa.io')
-    soonmi = User(username='Soon-Mi', email='soonmi@aa.io')
-    alissa = User(username='Alissa', email='alissa@aa.io')
+    ian = User(username='Ian', email='ian@aa.io', hashedPassword='123', firstName='pump',
+               zipcode='23798478', pinnedtweet=3, about='i am', profilepicture='jfhskjdhfksjhf')
+    javier = User(username='Javier', email='javier@aa.io', hashedPassword='133', firstName='pump',
+                  zipcode='23798478', pinnedtweet=3, about='i am', profilepicture='jfhskjdhfksjhf')
+    dean = User(username='Dean', email='dean@aa.io', hashedPassword='143', firstName='pump',
+                zipcode='23798478', pinnedtweet=3, about='i am', profilepicture='jfhskjdhfksjhf')
+    angela = User(username='Angela', email='angela@aa.io', hashedPassword='153', firstName='pump',
+                  zipcode='23798478', pinnedtweet=3, about='i am', profilepicture='jfhskjdhfksjhf')
+    soonmi = User(username='Soon-Mi', email='soonmi@aa.io', hashedPassword='163', firstName='pump',
+                  zipcode='23798478', pinnedtweet=3, about='i am', profilepicture='jfhskjdhfksjhf')
+    alissa = User(username='Alissa', email='alissa@aa.io', hashedPassword='173', firstName='pump',
+                  zipcode='23798478', pinnedtweet=3, about='i am', profilepicture='jfhskjdhfksjhf')
 
     db.session.add(ian)
     db.session.add(javier)
--- starter_app/models.py
+++ starter_app/models.py
@@ -2,14 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# follows = db.Table(
-#     "follows",
-#     db.Model.metadata,
-#     db.Column('followingid', db.Integer,
-#               db.ForeignKey("users.id"), primary_key=True),
-#     db.Column('followerid', db.Integer,
-#               db.ForeignKey("users.id"), primary_key=True)
-# )
+follows = db.Table(
+    "follows",
+    db.Model.metadata,
+    db.Column('following_id', db.Integer, db.ForeignKey("users.id"), primary_key=True),  # noqa
+    db.Column('follower_id', db.Integer, db.ForeignKey("users.id"), primary_key=True)  # noqa
+)
 
 
 class User(db.Model):
@@ -18,63 +16,68 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
-    hashedpassword = db.Column(db.Binary(100), nullable=False, unique=True)
-    firstname = db.Column(db.String(40), nullable=False, unique=True)
-    lastname = db.Column(db.String(40), nullable=False, unique=True)
+    hashed_password = db.Column(db.Binary(100), nullable=False, unique=True)
+    first_name = db.Column(db.String(40), nullable=False, unique=True)
+    last_name = db.Column(db.String(40), nullable=False, unique=True)
     zipcode = db.Column(db.Integer, nullable=False)
-    pinnedtweet = db.Column(db.Integer)
+    pinned_tweet = db.Column(db.Integer)
     about = db.Column(db.Text)
-    profilepic = db.Column(db.String)
+    profile_pic = db.Column(db.String)
 
-    tweets = db.relationship("User", back_populates="tweets")
-    likes = db.relationship("User", back_populates="likes")
-    replies = db.relationship("User", back_populates="replies")
+    tweets = db.relationship("Tweet", backref="user")
+    retweets = db.relationship("Tweet", backref="user")
+    likes = db.relationship("User", backref="user")
+    replies = db.relationship("User", backref="user")
+
+    following = db.relationship("User", secondary=follows, backref="user")
+    followers = db.relationship("User", secondary=follows, backref="user")
 
 
 class Tweet(db.Model):
     __tablename__ = 'tweets'
 
     id = db.Column(db.Integer, primary_key=True)
-    appuserid = db.Column(
-        db.Integer, db.ForeignKey('users.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    retweetedtweetid = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  # noqa
 
-    user = db.relationship("User", back_populates="users")
+    likes = db.relationship('Like', backref='tweet')
+    replies = db.relationship('Reply', backref='tweet')
+    retweets = db.relationship('Retweet', backref='tweet')
+
+
+class Retweet(db.Model):
+    __tablename__ = 'retweets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    tweet_id = db.Column(db.Integer, db.ForeignKey('tweets.id'))
+
+    likes = db.relationship('Like', backref='retweet')
 
 
 class Reply(db.Model):
     __tablename__ = 'replies'
 
     id = db.Column(db.Integer, primary_key=True)
-    appuserid = db.Column(
-        db.Integer, db.ForeignKey("users.id"), nullable=False)
-    tweetid = db.Column(db.Integer, db.ForeignKey("tweets.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)  # noqa
+    tweet_id = db.Column(db.Integer, db.ForeignKey("tweets.id"), nullable=False)  # noqa
     content = db.Column(db.Text, nullable=False)
 
-    user = db.relationship("User")
-    tweet = db.relationship("Tweet", back_populates="tweets")
+    likes = db.relationship('Like', backref='reply')
 
 
 class Like(db.Model):
     __tablename__ = 'likes'
 
     id = db.Column(db.Integer, primary_key=True)
-    appuserid = db.Column(
-        db.Integer, db.ForeignKey("users.id"), nullable=False)
-    liketweetid = db.Column(db.Integer, db.ForeignKey("tweets.id"), nullable=False)  # noqa
-
-    user = db.relationship("User")
-    tweet = db.relationship("Tweet")
+    liker = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    liked_tweet = db.Column(db.Integer, db.ForeignKey("tweets.id"), nullable=True)  # noqa
+    liked_retweet = db.Column(db.Integer, db.ForeignKey("retweets.id"), nullable=True)  # noqa
+    liked_reply = db.Column(db.Integer, db.ForeignKey("replies.id"), nullable=True)  # noqa
 
 
-class Follow(db.Model):
-    __tablename__ = 'follows'
+# class Follow(db.Model):
+#     __tablename__ = 'follows'
 
-    id = db.Column(db.Integer, primary_key=True)
-    followingid = db.Column(
-        db.Integer, db.ForeignKey("users.id"), nullable=False)
-    followerid = db.Column(
-        db.Integer, db.ForeignKey("users.id"), nullable=False)
-
-    follower = db.relationship("User", back_populates="follows")
+#     id = db.Column(db.Integer, primary_key=True)
+#     following_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)  # noqa
+#     follower_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)  # noqa
