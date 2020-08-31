f2a2f2f36a7c479043c32ffda215a88290deac02
Casey <innerforest7@gmail.com>
2020-08-30 19:13:03 -0700

    complete LeftPanel layout

--- group-project-starter/client/src/components/MainPage.js
+++ group-project-starter/client/src/components/MainPage.js
@@ -6,6 +6,9 @@ import Bell from '../images/Bell';
 import Envelope from '../images/Envelope';
 import Bookmark from '../images/Bookmark';
 import Paper from '../images/Paper';
+import Person from '../images/Person';
+import CirclesMore from '../images/CirclesMore';
+import DownCarrot from '../images/DownCarrot'
 
 const MainPage = () => {
 
@@ -57,6 +60,31 @@ const MainPage = () => {
             <div id={"paper-c"} className={"main-c__left-c__link-black"}>
               <Paper></Paper>
               <span>Lists</span>
+            </div>
+          </div>
+          <div className={"main-c__left-c"}>
+            <div id={"person-c"} className={"main-c__left-c__link-black"}>
+              <Person></Person>
+              <span>Profile</span>
+            </div>
+          </div>
+          <div className={"main-c__left-c"}>
+            <div id={"circles-more-c"} className={"main-c__left-c__link-black"}>
+              <CirclesMore></CirclesMore>
+              <span>More</span>
+            </div>
+          </div>
+          <div id={"main-c__left__bottom-c"} >
+            <div id={"tweet-button"}>
+              <span>Tweet</span>
+            </div>
+            <div id={"main-c__left__login"}>
+              <div className={"profile-bubble"} ></div>
+              <div className={"user-names"} >
+                <span className={"user-names__span1"}>Skeletor</span>
+                <span className={"user-names__span2"} >@Skeletor1</span>
+              </div>
+              <DownCarrot></DownCarrot>
             </div>
           </div>
         </div>
