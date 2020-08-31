d784af385fad970d474fca2eaef4c69eabec0f86
warrgiff <warrenbruceg@gmail.com>
2020-08-31 14:42:23 -0700

    forgot to save

--- flask/config.py
+++ flask/config.py
@@ -1,10 +1,11 @@
 import os
 
+
 class Config:
-  SECRET_KEY=os.environ.get('SECRET_KEY')
-  SQLALCHEMY_TRACK_MODIFICATIONS=False
-  SQLALCHEMY_DATABASE_URI="postgresql://twitter_user:password@localhost:9876/twitter_db"
-    # os.environ.get('FLASK_ENV')=="DEVELOPMENT" 
-    # if "postgresql://twitter_user:password@localhost:9876/twitter_db" 
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql://twitter_user:password@http://18.217.151.140:80/twitter_db"
+    # os.environ.get('FLASK_ENV')=="DEVELOPMENT"
+    # if "postgresql://twitter_user:password@localhost:9876/twitter_db"
     # else os.environ.get('DATABASE_URL') )
-  SQLALCHEMY_ECHO=True
+    SQLALCHEMY_ECHO = True
