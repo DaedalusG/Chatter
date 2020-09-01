d854f12149d9dde37bc5acd9d7544e2f435b203d
Casey <innerforest7@gmail.com>
2020-08-31 19:43:45 -0700

    add mScreen630

--- react/src/index.js
+++ react/src/index.js
@@ -4,6 +4,7 @@ import './styles/mainPage.css';
 import './styles/leftPanel.css';
 import './styles/rightPanel.css';
 import './styles/centerPanel.css';
+import './styles/mScreenheight630.css'
 import App from './App';
 
 ReactDOM.render(
