e0d312a9e9c0c5bc07bbaf0efc6e412694301e84
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-09-01 22:22:56 -0400

    local fixes after master push

--- flask/config.py
+++ flask/config.py
@@ -4,13 +4,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI=(
-      os.environ.get('FLASK_ENV')=="DEVELOPMENT" 
-      if 'postgresql://chatter_user:password@18.217.151.140:80/chatter_db'
-      else os.environ.get('DATABASE_URL') # container network
-    )
-    # "postgresql://twitter_user:password@localhost:9876/twitter_db" // for using local db
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql://chatter_db:password@3.133.142.3:80/chatter_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_ECHO = True
