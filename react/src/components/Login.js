5a1c9a4832b3e7b4593c4de6c19bfb0634539714
warrgiff <warrenbruceg@gmail.com>
2020-09-02 10:28:56 -0700

    merged branch with sign-up modal stuff

--- react/src/components/Login.js
+++ react/src/components/Login.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from "react";
-import { apiUrl, baseUrl, imageUrl } from "../config";
+import { apiUrl } from "../config";
 import SignUp from "./SignUp";
 import Bird from '../images/Bird';
 
@@ -10,60 +10,38 @@ import Bird from '../images/Bird';
 // }
 
 
-  
-const SignUpModal = ({ handleClose, show }) => {
-  const showHideClassName = show ? "modal is-active" : "modal";
-  
-  return (
-    <>
-      <div className={showHideClassName}>
-        <div className="signup-background">
-          <div className="signup__closeButton--container">
-            <button onClick={handleClose} className="signup__closeButton">
-              Close
-            </button>
-          </div>
-          <div className="signup-content--container">
-            <div className="signup-content">
-              <SignUp />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+
 
 const tryLogin = (email, password) => async () => {
-  const response = await fetch(`${apiUrl}/auth/login`, {
-    method: "put",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password }),
-  });
+    const response = await fetch(`${apiUrl}/auth/login`, {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+    });
 
-  if (response.ok) {
-    console.log("inside tryLogin: Success");
-  } else {
-    console.log("inside tryLogin: Response failure");
-  }
+    if (response.ok) {
+        console.log("inside tryLogin: Success");
+    } else {
+        console.log("inside tryLogin: Response failure");
+    }
 };
 
 const Login = (props) => {
-  const [signUpModal, setSignUpModal] = useState(false);
-  const [email, setEmail] = useState("demo@example.com");
-  const [password, setPassword] = useState("password");
+    const [signUpModal, setSignUpModal] = useState(false);
+    const [email, setEmail] = useState("demo@example.com");
+    const [password, setPassword] = useState("password");
 
-  const showSignUpModal = () => setSignUpModal(true);
-  const hideSignUpModal = () => setSignUpModal(false);
+    const showSignUpModal = () => setSignUpModal(true);
+    const hideSignUpModal = () => setSignUpModal(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log("reached HandleSubmit: ", email, password);
-    tryLogin(email, password);
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("reached HandleSubmit: ", email, password);
+        tryLogin(email, password);
+    };
 
-  const updateEmail = (e) => setEmail(e.target.value);
-  const updatePassword = (e) => setPassword(e.target.value);
+    const updateEmail = (e) => setEmail(e.target.value);
+    const updatePassword = (e) => setPassword(e.target.value);
 
     useEffect(() => {
         fetch('http://localhost:5000/auth/login/')
@@ -92,15 +70,15 @@ const Login = (props) => {
                             value={password}
                             onChange={updatePassword} />
                         <button className="login-button" type="submit">Log in</button>
-                        <a className="login-footer" href="/sign_up">Sign up for Chatter</a>
+                        {/* <a className="login-footer" href="/sign_up">Sign up for Chatter</a> */}
                         <div className="signup--container">
-            <SignUpModal show={signUpModal} handleClose={hideSignUpModal} />
-            <div className="signup__controls--container">
-              <button className="button" onClick={showSignUpModal}>
-                Sign Up
-              </button>
-            </div>
-          </div>
+                            <SignUpModal show={signUpModal} handleClose={hideSignUpModal} />
+                            <div className="signup__controls--container">
+                                <button className="button" onClick={showSignUpModal}>
+                                    Sign Up
+                                </button>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -108,5 +86,27 @@ const Login = (props) => {
     )
 }
 
+const SignUpModal = ({ handleClose, show }) => {
+    const showHideClassName = show ? "modal is-active" : "modal";
+
+    return (
+        <>
+            <div className={showHideClassName}>
+                <div className="signup-background">
+                    <div className="signup__closeButton--container">
+                        <button onClick={handleClose} className="signup__closeButton">
+                            Close
+            </button>
+                    </div>
+                    <div className="signup-content--container">
+                        <div className="signup-content">
+                            <SignUp />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default Login;
