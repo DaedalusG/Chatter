53c6fad1cbcdf84a7d6b4dd4e6388edb1a8a5b3a
Casey <innerforest7@gmail.com>
2020-09-03 09:47:35 -0700

    add flask/config

--- flask/config.py
+++ flask/config.py
@@ -5,11 +5,12 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get('FLASK_ENV') == "DEVELOPMENT"
-        if 'postgres://chatter_user:password@3.133.142.3:80/chatter_db'
-        else os.environ.get('DATABASE_URL')  # container network
-    )
+    # SQLALCHEMY_DATABASE_URI = (
+    #     os.environ.get('FLASK_ENV') == "DEVELOPMENT"
+    #     if 'postgres://chatter_user:password@3.133.142.3:80/chatter_db'
+    #     else os.environ.get('DATABASE_URL')  # container network
+    # )
     # "postgresql://twitter_user:password@localhost:9876/twitter_db" // for using local db
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
