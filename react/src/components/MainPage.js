202d3542eb64782e31445a44a1f5ec898f1867f2
warrgiff <warrenbruceg@gmail.com>
2020-09-03 16:32:36 -0700

    working user state

--- react/src/components/MainPage.js
+++ react/src/components/MainPage.js
@@ -5,19 +5,22 @@ import CenterPanelSwitch from './CenterPanelSwitch';
 import { apiUrl } from '../config.js'
 
 const MainPage = () => {
-  const [user, setUser] = useState(null)
+
+  const [user, setUser] = useState({})
 
   useEffect(() => {
     const getCurrentUser = async () => {
+      const token = window.localStorage.getItem('auth_token')
       const response = await fetch(apiUrl, {
         method: "GET",
         mode: "cors",
-        authorization: `Bearer ${window.localStorage.getItem('auth_token')}`,
+        headers: { "Authorization": `Bearer ${token}` },
       })
       if (!response.ok) {
         console.log("this will never happen. you can quote me")
       } else {
         const json = await response.json();
+        console.log(json)
         setUser(json);
       }
     }
